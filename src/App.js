91fcec725ea17ba57e53a9cac32526b1181165f5
GregoryMorantes <omalejandro7@hotmail.com>
2021-08-02 18:08:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
+import Spinner from './components/Spinner';
+import axios from 'axios';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`;
+
+const Footer = styled.footer`
+  background: linear-gradient(to right, rgba(78, 42, 186, .4), rgba(0, 4, 40, .8)); 
+  text-align: center;
+  font-size: 20px;
+  padding: 1rem;
+  border-radius: 12px;
+  margin: 1rem auto;
+  max-width: 880px;
+  color: #fff;
+`;
 
 function App() {
+
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda, guardarCriptomoneda] = useState('');
+  const [resultado, guardarResultado] = useState({});
+  const [cargando, guardarCargando] = useState(false);
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      //evitamos la ejecucion la primera vez
+      if(moneda === '') return;
+      
+      //consultar la API para obtener la cotizacion
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+      const resultado = await axios.get(url);
+
+      //Mostrar el spinner
+      guardarCargando(true);
+
+      //ocultar el spinner y mostrar el resultado
+      setTimeout(() => {
+        //Cambiar el estado de cargando
+        guardarCargando(false);
+
+        //Guardar cotizacion 
+        guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+      }, 3000);
+    }
+    cotizarCriptomoneda();
+  }, [moneda, criptomoneda]);
+
+  //mostrar spinner o resultado
+  const compnente = (cargando) ? <Spinner />: <Cotizacion resultado={resultado} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contenedor>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt="Imagen cripto"
+        />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario 
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+        />
+        {compnente}
+      </div>
+    </Contenedor>
+      <Footer>Creado por <b>Gregory Morantes</b></Footer>
     </div>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import Error from './Error';
+import axios from 'axios';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius:10px;
+    color: #fff;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326ac0;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = ({guardarMoneda, guardarCriptomoneda}) => {
+
+    //State del listado de las criptomonedas
+    const [ listadoCripto, guardarCriptomonedas ] = useState([]);
+    const [ error, guardarError ] = useState(false);
+
+    const MONEDAS = [
+        {codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
+        {codigo: 'EUR', nombre: 'Euro'},
+        {codigo: 'GBP', nombre: 'Libra Esterlina'}
+    ];
+
+    //Utilizar useMoneda
+    const [ moneda, SelectMonedas ] = useMoneda('Elige tu Moneada', '', MONEDAS);
+
+    //utilizar criptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '', listadoCripto);
+
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+
+    //cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //Validar si los campos estan llenos
+        if(moneda === '' || criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        //pasa los datos al componeten principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+
+    return ( 
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error ? <Error mensaje="Todos los campos son obligatorios" />:null}
+
+            <SelectMonedas />
+
+            <SelectCripto />
+
+            <Boton
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+    );
+}
+ 
+export default Formulario;
